378825e642ce74491636f2a067049bb5def58e7a
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-01-27 00:32:05 -0300

    started app

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = function () {
+    return (
+        <div>
+            <footer className="main-footer">Created by Ramon</footer>
+        </div>
+    );
+} 
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = function () {
+    return (
+        <div>
+            <h1 className="main-header">Create your to do list</h1>
+        </div>
+    );
+} 
+export default Header
