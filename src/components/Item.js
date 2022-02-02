60feacccb6c7700c15898a77a544893fddddb9b6
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-02-01 22:27:23 -0300

    quit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,15 @@
 //1 way to import an image, create a variable and use method "Require" to bring it
 const xbutton = require("./delete.png")
 
-
-
 function deleteItem(){
-    let paragraph = document.getElementById("itemParagraph")
+    let paragraph = document.getElementById("itemParagraph").innerText
     console.log(paragraph)
+
 
     //tem que usar um método slice para pegar o elemento acima somente a parte de texto
 
     //adicionar esse texto abaixo para pegar a div e então adicionar uma classe na div que deixe ela vermelha para mostrar que o item foi deletado
     //let deleted = document.getElementById("")
-
 }
 
 //apagar o item cujo id seja o mesmo que esta sendo clicado
@@ -19,7 +17,6 @@ function deleteItem(){
 
 const Item = function (props) {
     return (
-
         <div className="item" id={props.itemData.itemValue}>
             <p id="itemParagraph">
                 {props.itemData.itemValue}
@@ -27,7 +24,6 @@ const Item = function (props) {
                 {/* Then the variable image can be used */}  
           <img src={xbutton} alt="x button" className="xImage" onClick={deleteItem}></img>
         </div>
-
     );
 }
 export default Item
